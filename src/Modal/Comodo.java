3dd191ba3162be239d4fa30b6e58f8ab0597f36e
Rperoba <rodperoba@gmail.com>
2022-10-16 21:00:07 -0300

    .

--- src/Modal/Comodo.java
+++ src/Modal/Comodo.java
@@ -3,14 +3,17 @@ package Modal;
 public class Comodo extends Casa{
 	
 	String nome_comodo;
+	String arma;
 	Casa coordenadas_portas[];
 	int passagem_secreta;
 	
-	Comodo(String nome_comodo, Casa coordenadas_portas[],int passagem_secreta) {		
+	
+	Comodo(String nome_comodo,int passagem_secreta) {		
 		
 		this.nome_comodo = nome_comodo;
-		this.coordenadas_portas = coordenadas_portas;
+		//this.coordenadas_portas = coordenadas_portas; colocar depois de saber o mapa
 		this.passagem_secreta = passagem_secreta;
+		arma = Null;
 		// se n tem passagem secreta = 0, se tem, dois comodos tem passagem_secreta = 1 ou 2 ou 3 ...
 	}
 	
@@ -28,5 +31,15 @@ public class Comodo extends Casa{
 			count++;
 		}
 		return lista_de_comodos[count].get_coordenadas();
+	}
+	
+	protected boolean tem_arma() {
+		if arma == Null{
+			return False;
+		}
+		return True;
+	}
+	protected void atribui_arma(String = arma) {
+		this.arma = arma;
 	}
 }
