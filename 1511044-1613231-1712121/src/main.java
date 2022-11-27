5005aa3735b185fc10dbf1d193c221390cd0338a
Bach906 <70774853+Bach906@users.noreply.github.com>
2022-11-27 16:56:02 -0300

    mudancas na view

--- 1511044-1613231-1712121/src/main.java
+++ 1511044-1613231-1712121/src/main.java
@@ -14,22 +14,23 @@ public class main {
 		
 		System.out.println("Terminando");
 		
-		JFrame frame;
+		JFrame frame_menu;
+		JFrame frame_board;
+		JFrame frame_character_select;
+		JFrame frame_hand;
+		JFrame frame_notebook;
 		
-		// Testes das GUIs
-		// tirar comentario da tela que quiser testar e comentar a outra
-		// faltou implementar a funcionalidade dos botões, entao nao da pra testar o fluxo de telas ainda
-		// frame da mao do jogador faltou implementação, principalmente por depender mais da parte modal
-		// frame do tabuleiro precisa implementar a parte da cor do jogador dependendo de quem é o turno. versao atual apenas para testes
-		// tabuleiro tambem precisa ajustar os botoes para selecionar valor no dado
+		frame_menu = Detetive_frame.frame_start_menu();
+		frame_character_select = Detetive_frame.frame_character_select();
+		frame_board = Detetive_frame.frame_board();
+		frame_hand = Detetive_frame.frame_hand();
+		frame_notebook = Detetive_frame.frame_notebook();
 		
-		frame = Detetive_frame.frame_start_menu();
-//		frame = Detetive_frame.frame_character_select();
-//		frame = Detetive_frame.frame_board();
-//		frame = Detetive_frame.frame_hand();
-//		frame = Detetive_frame.frame_notebook();
-		
-		Detetive_frame.format_frame(frame);
+		Detetive_frame.format_frame(frame_menu);
+		Detetive_frame.format_frame(frame_character_select);
+		Detetive_frame.format_frame(frame_board);
+		Detetive_frame.format_frame(frame_hand);
+		Detetive_frame.format_frame(frame_notebook);
 	}
 
 }
--- 1511044-1613231-1712121/src/view/Detetive_frame.java
+++ 1511044-1613231-1712121/src/view/Detetive_frame.java
@@ -7,42 +7,54 @@ import java.io.*;
 
 public class Detetive_frame extends JFrame {
 	
-	public Detetive_frame(int width, int height, String title) {		
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setTitle(title);
-	}
+	public Detetive_frame() {}
 	
 	public static JFrame frame_start_menu() {
-		JFrame frame = new Detetive_frame(1200, 700, "Detetive - Menu Inicial");
-		JPanel p = Detetive_panel.main_menu_panel();
+		JFrame frame = new Detetive_frame();
+		JPanel p = new menu_panel();
+		
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setTitle("Detetive - Menu Inicial");
 		frame.getContentPane().add(p);
 		return frame;
 	}
 	
 	public static JFrame frame_character_select() {
-		JFrame frame = new Detetive_frame(1200, 700, "Detetive - Seleção de Personagem");
+		JFrame frame = new Detetive_frame();
 		JPanel p = Detetive_panel.character_panel();
+		
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setTitle("Detetive - Seleção de Personagem");
 		frame.getContentPane().add(p);
 		return frame;
 	}
 	
 	public static JFrame frame_board() {
-		JFrame frame = new Detetive_frame(1200, 700, "Detetive - Tabuleiro");
-		JPanel p = Detetive_panel.board_panel();
+		JFrame frame = new Detetive_frame();
+		JPanel p = new board_panel();
+		
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setTitle("Detetive - Tabuleiro");
 		frame.getContentPane().add(p);
 		return frame;
 	}
 	
 	public static JFrame frame_hand() {
-		JFrame frame = new Detetive_frame(500, 500, "Detetive - Mão do jogador");
-		JPanel p = Detetive_panel.hand_panel();
+		JFrame frame = new Detetive_frame();
+		JPanel p = new hand_panel();
+		
+		frame.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		frame.setTitle("Detetive - Mão do jogador");
 		frame.getContentPane().add(p);
 		return frame;
 	}
 	
 	public static JFrame frame_notebook() {
-		JFrame frame = new Detetive_frame(500, 500, "Detetive - Bloco de Notas");
-		JPanel p = Detetive_panel.notebook_panel();
+		JFrame frame = new Detetive_frame();
+		JPanel p = new note_panel();
+		
+		frame.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		frame.setTitle("Detetive - Bloco de Notas");
 		frame.getContentPane().add(p);
 		return frame;
 	}
